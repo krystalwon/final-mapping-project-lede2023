ddb3e894364389d9fc3e1d283081d0128d0483d6
Krystal Won <krystal_w@utexas.edu>
2025-03-25 00:18:57 -0400

    v2025

--- config.js
+++ config.js
@@ -1,15 +1,6 @@
-// let topTitleDiv = "";
-
-// let titleDiv = "";
-
-// let bylineDiv = "";
-
-// let descriptionDiv = "";
-// let footerDiv = "";
-
 // Number of slides that will drive (more = smoother)
 // If this doesn't match the number of slides named 'drive-slide' in config below you will not complete the full journey
-var driveSlides = 5;
+var driveSlides = 4;
 
 // Number of points on drive route (more = higher quality, but slower to process)
 var driveSmoothness = 300;
@@ -21,30 +12,18 @@ var driveTime = driveSlides*driveSmoothness;
 var followPoint = true;
 
 // ...If so, what zoom, pitch, and bearing should be used to follow?
-var followZoomLevel = 25;
-var followBearing = -120;
-var followPitch = 60;
+var followZoomLevel = 16;
+var followBearing = -30;
+var followPitch = 45;
 
 // to add 'driving' slides just make sure to add 'drive to beginning of slide id'
 // you also need to add a running Order total to the end of each 'drive-slide', (ex. drive-slide-0, drive-slide-1, drive-slide-2, etc.)
 var config = {
     style: 'mapbox://styles/mapbox/outdoors-v10',
-    // style: 'mapbox://styles/mapbox/satellite-v8',
-    // style: 'mapbox://styles/mapbox/outdoors-v10',
-    accessToken: 'pk.eyJ1IjoiZGFsaXZhbnBpY2Fzc29udW0yIiwiYSI6ImNsamVkNjBmbDBhZjYzcnRjaGdpd3B3ZTMifQ.MfHoo1ICi_XA1QUlzg6uhA',
+    accessToken: 'pk.eyJ1IjoiZGFsaXZhbnBpY2Fzc29udW0yIiwiYSI6ImNtMTg0MGwzcTBxZnoybHB3dGsxOGs3N3MifQ.ubimGmdKCe9GNuSwkk5T6g',
     showMarkers: false,
     theme: 'light',
     alignment: 'left',
-    // title: 'The Title Text of this Story',
-    // subtitle: 'A descriptive and interesting subtitle to draw in the reader',
-    // byline: 'By a Digital Storyteller',
-    // footer: 'Source: source citations, etc.',
-    // topTitle: topTitleDiv,
-    // title: titleDiv,
-    // subtitle: "Sergio's Morning Commute",
-    // byline: bylineDiv,
-    // description: descriptionDiv,
-    // footer: footerDiv,
     chapters: [
         {
             id: 'drive-slide-0',
@@ -58,8 +37,6 @@ var config = {
                 pitch: 63.00,
                 bearing: -121.60
             },
-            // mapAnimation: 'flyTo',
-            // callback: '',
             onChapterEnter: [],
             onChapterExit: []
         },
@@ -68,15 +45,14 @@ var config = {
             title: 
                 'This route always holds a special place in my heart',
             description: 
-                '<p>This mere 40-minute stroll can feel like a journey through the very essence of the city.</p>'+'<p>Beginning in the student-rich enclave of West Campus, our path would meander towards the heart of downtown.</p>'+'<p>En route, the narrative of Austin unfolds: there\'s the stalwart presence of Texas\' most eminent independent bookstore, a record shop that has defiantly spun vinyl since the 1980s, and a local coffee roastery queued with young people who just got off from yoga class.</p>'+'<h3>To me, it is a walk that, in its simplicity, captures the complex tapestry of Austin.</h3>',
+                '<p>This mere 40-minute stroll can feel like a journey through the very essence of the city.</p>'+'<p>Beginning in the student-rich enclave of West Campus, our path would meander towards the heart of downtown.</p>'+'<h3>To me, it is a walk that, in its simplicity, captures the complex tapestry of Austin.</h3>',
             location: {
-                // center: [-97.75260, 30.28611],
-                // zoom: 15.03,
-                // pitch: 38.00,
-                // bearing: -146.40  // location information is from the drive route
+                center: [-97.74590, 30.28211],
+                zoom: 15.03,
+                pitch: 38.00,
+                bearing: -146.40
             },
             mapAnimation: 'flyTo',
-            callback: '',
             onChapterEnter: [],
             onChapterExit: []
         },
@@ -84,90 +60,126 @@ var config = {
             id: 'drive-slide-02',
             title: 'Well...sometime we do encounter random roadblocks :|',
             image: './charts/images/roadblock.png',
-            // description: 
-            //     '<p>Delivery pay: $2.77</p>' + 
-            //     '<p>Mileage: $0.23 for biking 0.958 miles</p>' + 
-            //     '<p>Tip: $2.70</p>' + '<hr>' + 
-            //     '<p><b>Order total: $5.70</b></p>' + 
-            //     '<h3>Total: $19.76</h3>',
-            location: {},
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'drive-slide-03',
-            title: "Picking up some nostalgia moment :)",
-            image: './charts/images/waterloo.png',
-            description: 
-                '<p>Now we arrives at the testament to Austin\'s rich musical lineage: <b>Waterloo Records.</b></p>'
-                +'<p>It\'s more than just a record store for me—<b>it\'s my personal sanctuary</b>, a place where vinyl and nostalgia spin in harmonious synchrony. In a world rapidly turning Spotify, this shop stands as a defiant ode to the tactile joy of music, making it <b>the finest record store on the planet</b><i> (in my own pov)</i>!</p>',
-            location: {},
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'slide-01',
-            title: 'Gotta Introduce Some of My Favs at All Time:',
-            image: './charts/images/vinyl.png', 
-            location: {},
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-        {
-            id: 'slide-02',
-            title: 'This Is A Busy Block: Where Vinyl Meets Verse',
-            image: './charts/images/bookpeople.png',
-            description: 
-                '<p>Right across the record store stands the largest independent bookstore in the state. <b>BookPeople</b> has been a staple of Austin\'s literary scene since 1970. They are taking all approaches to make the city a beloved spot for book lovers and a cultural hub in Austin. </p>',
-            location: {},
-            onChapterEnter: [],
-            onChapterExit: []
-        },
-
-        // You can add a normal slide in the middle of the drive slides to take a break
-        {
-            id: 'slide-03',
-            image: './charts/images/bannedbooks.png',
-            description: '<p>At a time when book banning was sweeping the nation, BookPeople took a defiant stand.</p>'+'<p>Proudly, on the first floor, they showcased a vast display of all the banned titles, reminding everyone who entered about the power of books and the importance of intellectual freedom.</p>',
             location: {
+                center: [-97.74990, 30.27611],
+                zoom: 16.5,
+                pitch: 50.00,
+                bearing: -120.00
             },
             onChapterEnter: [],
             onChapterExit: []
         },
         {
-            id: 'drive-slide-04',
-            title: 'Highlight of the Day!!!!!!!',
-            image: './charts/images/merit.png',
+            id: 'slide-01',
+            title: "Picking up some nostalgia moment :)",
+            image: './charts/images/waterloo.png',
             description: 
-                '<p>Before hitting up Trader Joe\'s, we always made a stop at <b>Merit Coffee</b>. Their pastries and coffee became our Friday morning must-haves.</p>' + '<p>We became such regulars that we befriended (almost) all the baristas.</p>',
-            location: {center: [-97.75210, 30.26761]},
+                '<p>Now we arrives at the testament to Austin\'s rich musical lineage: <b>Waterloo Records.</b></p>'
+                +'<p>It\'s more than just a record store for me—<b>it\'s my personal sanctuary</b>, a place where vinyl and nostalgia spin in harmonious synchrony. In a world rapidly turning Spotify, this shop stands as a defiant ode to the tactile joy of music, making it <b>the finest record store on the planet</b><i> (in my own pov!)</i>!</p>',
+            location: {
+                center: [-97.75023, 30.27142],
+                zoom: 16.50,
+                pitch: 50.00,
+                bearing: -120.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'slide-02',
+            title: 'Gotta Introduce Some of My Favs at All Time:',
+            image: './charts/images/vinyl.png', 
+            location: {
+                center: [-97.75023, 30.27142], 
+                zoom: 16.50,
+                pitch: 50.00,
+                bearing: -118.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'slide-03',
+            title: 'This Is A Busy Block: Where Vinyl Meets Verse',
+            image: './charts/images/bookpeople.png',
+            description: 
+                '<p>Right across the record store stands the largest independent bookstore in the state. <b>BookPeople</b> has been a staple of Austin\'s literary scene since 1970. They are taking all approaches to make the city a beloved spot for book lovers and a cultural hub in Austin. </p>',
+            location: {
+                center: [-97.75111, 30.27125], // BookPeople
+                zoom: 16.50,
+                pitch: 50.00,
+                bearing: -122.00
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'drive-slide-03',
+            image: './charts/images/bannedbooks.png',
+            description: '<p>At a time when book banning was sweeping the nation, BookPeople took a defiant stand.</p>'+'<p>Proudly, on the first floor, they showcased a vast display of all the banned titles, reminding everyone who entered about the power of books and the importance of intellectual freedom.</p>',
+            location: {
+                center: [-97.75111, 30.27125], 
+                zoom: 16.50,
+                pitch: 50.00,
+                bearing: -120.00
+            },
             onChapterEnter: [],
             onChapterExit: []
         },
         {
             id: 'slide-04',
-            title: 'According to an uncompleted statistic from my data...',
-            image: './charts/images/coffee.png',
-            location: {},
+            title: 'Highlight of the Day!!!!!!!',
+            image: './charts/images/merit.png',
+            description: 
+                '<p>Before hitting up Trader Joe\'s, we always made a stop at <b>Merit Coffee</b>. Their pastries and coffee became our Friday morning must-haves.</p>' + '<p>We became such regulars that we befriended (almost) all the baristas.</p>',
+            location: {
+                center: [-97.75, 30.26659], 
+                zoom: 16.03,
+                pitch: 38.00,
+                bearing: -146.40
+            },
             onChapterEnter: [],
             onChapterExit: []
         },
         {
             id: 'slide-05',
-            title: 'Another hobby of us is \'Dog Watching\'',
-            image: './charts/images/dog.png',
-            // description:,
-            location: {center: [-97.75210, 30.26761]},
+            title: 'According to an uncompleted statistic from my complexed album dataset...',
+            image: './charts/images/coffee.png',
+            location: {
+                center: [-97.75, 30.26659], 
+                zoom: 16.03,
+                pitch: 38.00,
+                bearing: -146.40
+            },
             onChapterEnter: [],
             onChapterExit: []
         },
         {
-            id: 'drive-slide-05',
-            title: 'Around noon time, we fiiiinallly make it to the Trader Joe\'s.',
+            id: 'slide-06',
+            title: 'Another hobby of us is \'Dog Watching\'',
+            image: './charts/images/dog.png',
+            location: {
+                center: [-97.75, 30.26659], 
+                zoom: 16.03,
+                pitch: 38.00,
+                bearing: -146.40
+            },
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+
+        {
+            id: 'drive-slide-07',
+            title: 'Around noon time, we fiiiinallly move our foot into Trader Joe\'s. 🍾',
             image: './charts/images/flower.png',
             description:  
-                '<p>But in addition to filling our carts with food and drinks, we\'d also make a special pick: our <b>"flower of the week"</b> to conclude our Friday.</p>',
-            location: {},
+                '<p>But in addition to filling our carts with eggs and milk, we\'d also make a special pick: our <b>"flower of the week"</b> to conclude our Friday.</p>',
+            location: {
+                center: [-97.75171, 30.26627],
+                zoom: 16.03,
+                pitch: 38.00,
+                bearing: -146.40
+            },
             onChapterEnter: [],
             onChapterExit: []
         },  
--- js/drive.js
+++ js/drive.js
@@ -1,3 +1,9 @@
+if (!turf.length && turf.lineDistance) {
+    turf.length = turf.lineDistance;
+} else if (!turf.lineDistance && turf.length) {
+    turf.lineDistance = turf.length;
+}
+
 var routeData = [];
 
 var geojsonPoint = {
@@ -14,7 +20,14 @@ var geojsonPoint = {
 };
 
 function createLine() {
+    // Check if routeData is loaded and has the expected structure
+    if (!routeData || !routeData.features || !routeData.features[0] || !routeData.features[0].geometry || !routeData.features[0].geometry.coordinates) {
+        console.error('Route data is not properly loaded or structured');
+        return;
+    }
 
+    console.log('Creating line with route data:', routeData);
+    
     // get the coordinates of the line you want to highlight
     let extentArray = routeData.features[0].geometry.coordinates;
     
@@ -37,7 +50,7 @@ function createLine() {
     for(let i = 0; i <= rects; i++) {
 
         // calculate point location for each segment
-        const pointonline = turf.along(line, (segments * i));
+        const pointonline = turf.along(line, (segments * i), {units: units});
 
         // push new x,y
         let newX = pointonline.geometry.coordinates[0];
@@ -51,8 +64,8 @@ function createLine() {
 
             // if you want to follow the point...
             if (followPoint === true) {
-            	map.setCenter([newX, newY]);
-        	}
+                map.setCenter([newX, newY]);
+            }
 
             map.getSource('pointSource').setData(initPoint);
         }
@@ -62,16 +75,29 @@ function createLine() {
             map.getSource('lineSource').setData(geojsonPoint);
         }
     }
-
 }
 
 function changeCenter(index) {
+    // Make sure we have coordinates first
+    if (!geojsonPoint.features[0].geometry.coordinates.length) {
+        console.error('No coordinates available for animation');
+        return;
+    }
 
-    // Set center to a subsample of the line, say every 10th or 25th
-    let subsampleIndex = 100;
+    // Make sure the index doesn't exceed our coordinates array length
+    if (index >= geojsonPoint.features[0].geometry.coordinates.length) {
+        index = geojsonPoint.features[0].geometry.coordinates.length - 1;
+    }
 
-    let currentJson = geojsonPoint.features[0].geometry.coordinates.slice(0,index);
+    let currentJson = geojsonPoint.features[0].geometry.coordinates.slice(0, index);
     let center = geojsonPoint.features[0].geometry.coordinates[index];
+    
+    // Check if center exists
+    if (!center) {
+        console.error('Invalid center point at index:', index);
+        return;
+    }
+    
     let centerX = center[0];
     let centerY = center[1];
     let movingLine = {
@@ -85,16 +111,213 @@ function changeCenter(index) {
         }]
     };
     let movingPoint = turf.point([centerX, centerY]);
-    map.getSource('lineSource').setData(movingLine);
-    map.getSource('pointSource').setData(movingPoint);
+    
+    try {
+        map.getSource('lineSource').setData(movingLine);
+        map.getSource('pointSource').setData(movingPoint);
 
-    // if you want to follow the point...
-    if (followPoint === true) {
-      if (index % subsampleIndex == 0) {
-        console.log("changeCenter(index) = ", index, center)
-        map.jumpTo({
-  	        center: [centerX, centerY]
-  	    });
-      }
+        // if you want to follow the point...
+        if (followPoint === true) {
+            // Only transition at certain intervals for performance
+            // Adjust this number to control transition frequency
+            let transitionInterval = 50; 
+            
+            if (index % transitionInterval == 0) {
+                console.log("changeCenter(index) = ", index, center);
+                
+                // Use flyTo instead of jumpTo for smooth animation
+                map.flyTo({
+                    center: [centerX, centerY],
+                    zoom: followZoomLevel,
+                    bearing: followBearing,
+                    pitch: followPitch,
+                    speed: 0.5, // Controls animation speed (lower = slower)
+                    curve: 1, // Controls animation curve (higher = more pronounced)
+                    essential: true // Animation is considered essential for the user experience
+                });
+            }
+        }
+    } catch (error) {
+        console.error('Error updating map sources:', error);
     }
 }
+
+// zoom to specific locations
+function zoomToLocation(locationId, zoomLevel, bearing, pitch) {
+    // Find the location in config.chapters
+    const location = config.chapters.find(chap => chap.id === locationId);
+    
+    if (location && location.location && location.location.center) {
+        map.flyTo({
+            center: location.location.center,
+            zoom: zoomLevel || 15, // Default zoom if not specified
+            bearing: bearing || 0, // Default bearing if not specified
+            pitch: pitch || 60, // Default pitch if not specified
+            duration: 2000, // Animation duration in milliseconds
+            essential: true
+        });
+    } else {
+        console.error(`Location with ID "${locationId}" not found or has no center coordinates`);
+    }
+}
+
+
+function setupMapClickCoordinates() {
+    map.on('click', function(e) {
+      var coordinates = e.lngLat;
+      console.log(`Clicked coordinates: center: [${coordinates.lng.toFixed(5)}, ${coordinates.lat.toFixed(5)}]`);
+      
+      // Copy to clipboard for easy pasting
+      navigator.clipboard.writeText(`center: [${coordinates.lng.toFixed(5)}, ${coordinates.lat.toFixed(5)}]`)
+        .then(() => console.log('Coordinates copied to clipboard'))
+        .catch(err => console.error('Error copying to clipboard:', err));
+        
+      // Show a popup with coordinates
+      new mapboxgl.Popup()
+        .setLngLat(coordinates)
+        .setHTML(`<strong>Coordinates:</strong><br>[${coordinates.lng.toFixed(5)}, ${coordinates.lat.toFixed(5)}]<br><small>(Copied to clipboard)</small>`)
+        .addTo(map);
+    });
+}
+
+
+function verifySlideOrder() {
+    console.log("=== Verifying Slide Order ===");
+    console.log("(Check that these locations make sense in sequence)");
+    
+    // Extract just the necessary info for debugging
+    const slideInfo = config.chapters.map((chapter, index) => {
+      return {
+        index: index,
+        id: chapter.id,
+        title: chapter.title || "(No title)",
+        location: chapter.location && chapter.location.center ? 
+          `[${chapter.location.center[0]}, ${chapter.location.center[1]}]` : 
+          "No location"
+      };
+    });
+    
+    console.table(slideInfo);
+    
+    // Check for route continuity
+    console.log("\n=== Checking Route Continuity ===");
+    
+    // Only look at drive slides
+    const driveSlides = config.chapters.filter(chapter => 
+      chapter.id.startsWith('drive-slide'));
+    
+    console.log(`Found ${driveSlides.length} drive slides (driveSlides variable is set to ${driveSlides})`);
+    
+    if (driveSlides.length > 0) {
+      console.log("\nDrive Slides Sequence:");
+      driveSlides.forEach((slide, i) => {
+        if (i > 0 && driveSlides[i-1].location && slide.location) {
+          const prev = driveSlides[i-1].location.center;
+          const curr = slide.location.center;
+          const distance = calculateDistance(prev[1], prev[0], curr[1], curr[0]);
+          console.log(`${slide.id}: ${slide.title || "(No title)"} - Distance from previous: ${distance.toFixed(2)} km`);
+        } else {
+          console.log(`${slide.id}: ${slide.title || "(No title)"}`);
+        }
+      });
+    }
+  }
+  
+  // Simple distance calculation between two points (Haversine formula)
+  function calculateDistance(lat1, lon1, lat2, lon2) {
+    const R = 6371; // Radius of the earth in km
+    const dLat = deg2rad(lat2-lat1);
+    const dLon = deg2rad(lon2-lon1); 
+    const a = 
+      Math.sin(dLat/2) * Math.sin(dLat/2) +
+      Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * 
+      Math.sin(dLon/2) * Math.sin(dLon/2); 
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    const d = R * c; // Distance in km
+    return d;
+}
+
+
+
+function addDebugControls() {
+    // Create a container for debug controls
+    const controlDiv = document.createElement('div');
+    controlDiv.className = 'debug-controls';
+    controlDiv.style.position = 'absolute';
+    controlDiv.style.top = '10px';
+    controlDiv.style.right = '10px';
+    controlDiv.style.zIndex = '1000';
+    controlDiv.style.backgroundColor = 'white';
+    controlDiv.style.padding = '10px';
+    controlDiv.style.borderRadius = '4px';
+    controlDiv.style.boxShadow = '0 2px 4px rgba(0,0,0,0.2)';
+    
+    // Add a button for each key location
+    const locations = [
+      { name: "Waterloo", coords: [-97.75023, 30.27142] },
+      { name: "BookPeople", coords: [-97.75111, 30.27125] },
+      { name: "Merit Coffee", coords: [-97.74793, 30.26659] },
+      { name: "Trader Joe's", coords: [-97.75097, 30.26972] }
+    ];
+    
+    // Add a title
+    const title = document.createElement('div');
+    title.textContent = 'Test Locations';
+    title.style.fontWeight = 'bold';
+    title.style.marginBottom = '8px';
+    controlDiv.appendChild(title);
+    
+    // Add buttons for each location
+    locations.forEach(loc => {
+      const button = document.createElement('button');
+      button.textContent = loc.name;
+      button.style.display = 'block';
+      button.style.margin = '4px 0';
+      button.style.padding = '4px 8px';
+      
+      button.addEventListener('click', () => {
+        map.flyTo({
+          center: loc.coords,
+          zoom: 18,
+          pitch: 60,
+          bearing: 0,
+          duration: 1500
+        });
+      });
+      
+      controlDiv.appendChild(button);
+    });
+    
+    // Add a toggle for showing coordinates on click
+    const toggleDiv = document.createElement('div');
+    toggleDiv.style.marginTop = '8px';
+    
+    const toggle = document.createElement('input');
+    toggle.type = 'checkbox';
+    toggle.id = 'coord-toggle';
+    
+    const label = document.createElement('label');
+    label.htmlFor = 'coord-toggle';
+    label.textContent = 'Show coords on click';
+    label.style.marginLeft = '4px';
+    label.style.fontSize = '12px';
+    
+    toggle.addEventListener('change', (e) => {
+      if (e.target.checked) {
+        setupMapClickCoordinates();
+      }
+    });
+    
+    toggleDiv.appendChild(toggle);
+    toggleDiv.appendChild(label);
+    controlDiv.appendChild(toggleDiv);
+    
+    // Add to map
+    document.body.appendChild(controlDiv);
+  }
+  
+  // Call this after map is loaded:
+  // map.on('load', function() {
+  //   ...existing code...
+  //   addDebugControls();
+  // });
